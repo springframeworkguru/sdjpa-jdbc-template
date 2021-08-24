ee6a33b29dcf30377747dadd5484615a6e53b192
John Thompson <john@springframework.guru>
2021-08-24 07:57:47 -0400

    adding update author

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -41,7 +41,11 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+
+        jdbcTemplate.update("UPDATE author SET first_name = ?, last_name = ? WHERE id = ?",
+                author.getFirstName(), author.getLastName(), author.getId());
+
+        return this.getById(author.getId());
     }
 
     @Override
