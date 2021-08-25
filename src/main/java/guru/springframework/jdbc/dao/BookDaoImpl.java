8984bf927b3990185365c22b9bdcff7f1b7b081a
John Thompson <john@springframework.guru>
2021-08-25 09:17:20 -0400

    adding book dao impl

---
+++ src/main/java/guru/springframework/jdbc/dao/BookDaoImpl.java
@@ -0,0 +1,54 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Book;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 8/25/21.
+ */
+@Component
+public class BookDaoImpl implements BookDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Book getById(Long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM book where id = ?", getBookMapper(), id);
+    }
+
+    @Override
+    public Book findBookByTitle(String title) {
+        return jdbcTemplate.queryForObject("SELECT * FROM book where title = ?", getBookMapper(), title);
+    }
+
+    @Override
+    public Book saveNewBook(Book book) {
+        jdbcTemplate.update("INSERT INTO book (isbn, publisher, title, author_id) VALUES (?, ?, ?, ?)",
+                book.getIsbn(), book.getPublisher(), book.getTitle(), book.getAuthorId());
+
+        Long createdId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+
+        return this.getById(createdId);
+    }
+
+    @Override
+    public Book updateBook(Book book) {
+        jdbcTemplate.update("UPDATE book set isbn = ?, publisher = ?, title = ?, author_id = ? where id = ?",
+                book.getIsbn(), book.getPublisher(), book.getTitle(), book.getAuthorId(), book.getId());
+
+        return this.getById(book.getId());
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        jdbcTemplate.update("DELETE from book where id = ?", id);
+    }
+
+    private BookMapper getBookMapper(){
+        return new BookMapper();
+    }
+}
