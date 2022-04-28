28f779157f3272e987b052e6ff4a9d8e263c7da5
chukwudozie <emkychuks062@gmail.com>
2022-04-28 12:55:07 +0100

    CRUD for Author

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -1,6 +1,8 @@
 package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,28 +10,44 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+    private final JdbcTemplate template;
+
+    public AuthorDaoImpl(JdbcTemplate template) {
+        this.template = template;
+    }
+
     @Override
     public Author getById(Long id) {
-        return null;
+       return template.queryForObject("SELECT * FROM author WHERE id = ?",getRowMapper(),id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        return template.queryForObject("SELECT * FROM author WHERE first_name = ? and last_name = ?"
+                ,getRowMapper(),firstName,lastName);
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        template.update("INSERT INTO author (first_name, last_name) VALUES (?, ?)",
+                author.getFirstName(), author.getLastName());
+        Long savedId = template.queryForObject("SELECT LAST_INSERT_ID()",Long.class);
+        return this.getById(savedId);
     }
 
     @Override
     public Author updateAuthor(Author author) {
-        return null;
+        template.update("UPDATE author SET first_name = ?, last_name = ? WHERE id = ?",
+                author.getFirstName(),author.getLastName(),author.getId());
+        return this.getById(author.getId());
     }
 
     @Override
     public void deleteAuthorById(Long id) {
+        template.update("DELETE FROM author WHERE id = ?", id);
+    }
 
+    private RowMapper<Author> getRowMapper(){
+        return new AuthorMapper();
     }
 }
---
+++ src/main/java/guru/springframework/jdbc/dao/AuthorMapper.java
@@ -0,0 +1,23 @@
+package guru.springframework.jdbc.dao;
+
+import guru.springframework.jdbc.domain.Author;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AuthorMapper implements RowMapper<Author> {
+
+    /**
+     *This method maps the rows of a database to an object of the author class
+     * by converting a result set from the database into a domain object.
+     */
+    @Override
+    public Author mapRow(ResultSet resultSet, int i) throws SQLException {
+        Author author = new Author();
+        author.setId(resultSet.getLong("id"));
+        author.setFirstName(resultSet.getString("first_name"));
+        author.setLastName(resultSet.getString("last_name"));
+        return author;
+    }
+}
